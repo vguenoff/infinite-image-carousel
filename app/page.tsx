566423157810aa3f26eb0a7d222cb9b33475d34a
Vasil Genov <vasil.genov@psinteractive.io>
2024-05-22 13:28:39 +0300

    feat: improve styling and content

--- app/page.tsx
+++ app/page.tsx
@@ -18,16 +18,21 @@ export default async function Home() {
     <main>
       <h1>Boston Public Library</h1>
       <p>
-        Considered a pioneer of public library service in the United States, the
-        Boston Public Library is among the three largest collections in the
-        country and is committed to be ‘Free for All’.
+        <strong>
+          Considered a pioneer of public library service in the United States,
+          the Boston Public Library is among the three largest collections in
+          the country and is committed to be ‘Free for All’.
+        </strong>
       </p>
       <Carousel {...{ data }} />
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
-        illum deleniti explicabo quisquam corporis enim in, consectetur veniam
-        adipisci quaerat nam modi voluptatum iure obcaecati magni repudiandae
-        aliquam? Soluta, ad.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
       </p>
     </main>
   )
